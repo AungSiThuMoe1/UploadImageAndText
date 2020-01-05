04c34671a7c762e8b2b19ee064df7726ce1b1bce
AungSiThuMoe1 <aungsithumoe23@gmail.com>
2020-01-06 05:27:29 +0630

    imageupload1.php
    php file for upload image

---
+++ imageupload1.php
@@ -0,0 +1,13 @@
+<?php
+
+$result = array("success" => $_FILES["file"]["name"]);
+$uploads_dir = 'uploads/';
+    $file_path = $uploads_dir . basename( $_FILES['file']['name']);
+if(move_uploaded_file($_FILES['file']['tmp_name'],$file_path)) {
+    $result = array("success" => "File successfully uploaded");
+} else{
+    $result = array("success" => "error uploading file");
+}
+echo json_encode($result, JSON_PRETTY_PRINT);
+
+?>
